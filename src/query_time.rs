80773f66bb68d07428a4d98935f7f2403a69dd5f
Ted Driggs <ted.driggs@outlook.com>
2021-02-16 07:05:49 -0800

    Rework QueryTime to remove crate::Error dependency

--- src/query_time.rs
+++ src/query_time.rs
@@ -1,9 +1,34 @@
-use crate::Result;
-use serde::{Deserialize, Serialize};
-use std::str::FromStr;
+use serde::{Serialize, Serializer};
+use std::{fmt, num::NonZeroU64};
 
-/// Represents an absolute or relative time sent to an ExtraHop appliance
+#[derive(Debug, Clone)]
+enum Inner {
+    Timestamp(NonZeroU64),
+    Now,
+    MsAgo(NonZeroU64),
+    RelativeUnits(String),
+}
+
+impl Serialize for Inner {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        match self {
+            Inner::Now => 0.serialize(serializer),
+            Inner::Timestamp(ts) => ts.serialize(serializer),
+            Inner::MsAgo(ms) => ((ms.get() as i64) * -1).serialize(serializer),
+            Inner::RelativeUnits(string) => string.serialize(serializer),
+        }
+    }
+}
+
+/// Represents an absolute or relative time sent to an ExtraHop API
 /// as part of a query.
+///
+/// # Default
+/// By default, `QueryTime` refers to the current time as known to the called API;
+/// this is represented in API requests as `0`.
 ///
 /// # Usage
 /// `QueryTime` instances should generally be constructed using the conversion
@@ -11,11 +36,17 @@ use std::str::FromStr;
 ///
 /// ```rust
 /// # use extrahop::QueryTime;
-/// let _time: QueryTime = (-30000 as i64).into();
+/// let _time: QueryTime = (-30000i64).into();
 /// let _other: QueryTime = "-30m".into();
 /// ```
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct QueryTime(String);
+#[derive(Clone, Serialize)]
+pub struct QueryTime(Inner);
+
+impl fmt::Debug for QueryTime {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self.0.fmt(f)
+    }
+}
 
 impl QueryTime {
     /// Returns `true` if the query time is relative to the appliance's "now".
@@ -24,10 +55,13 @@ impl QueryTime {
     /// ```rust
     /// # use extrahop::QueryTime;
     /// assert!(QueryTime::from("-30m").is_relative());
-    /// assert!(QueryTime::from(0).is_relative());
+    /// assert!(QueryTime::from(0u64).is_relative());
     /// ```
     pub fn is_relative(&self) -> bool {
-        self.0 == "0" || self.0.starts_with('-')
+        match self.0 {
+            Inner::Timestamp(_) => false,
+            Inner::Now | Inner::MsAgo(_) | Inner::RelativeUnits(_) => true,
+        }
     }
 
     /// Returns `true` if the query time is measured from the Unix epoch.
@@ -38,13 +72,28 @@ impl QueryTime {
 
 impl Default for QueryTime {
     fn default() -> Self {
-        Self("0".to_string())
+        Self(Inner::Now)
+    }
+}
+
+impl From<u64> for QueryTime {
+    fn from(val: u64) -> Self {
+        if let Some(ts) = NonZeroU64::new(val) {
+            Self(Inner::Timestamp(ts))
+        } else {
+            Self(Inner::Now)
+        }
     }
 }
 
 impl From<i64> for QueryTime {
     fn from(val: i64) -> Self {
-        Self(format!("{}", val))
+        match val {
+            0 => Self(Inner::Now),
+            x if x > 0 => Self(Inner::Timestamp(NonZeroU64::new(x as u64).unwrap())),
+            x if x < 0 => Self(Inner::MsAgo(NonZeroU64::new(x.abs() as u64).unwrap())),
+            _ => unreachable!(),
+        }
     }
 }
 
@@ -58,14 +107,41 @@ impl<'a> From<&'a str> for QueryTime {
 /// number if doing so would not change readability in the serialized form.
 impl From<String> for QueryTime {
     fn from(val: String) -> Self {
-        Self(val)
+        Self(Inner::RelativeUnits(val))
     }
 }
 
-impl FromStr for QueryTime {
-    type Err = crate::Error;
+#[cfg(test)]
+mod tests {
+    use super::QueryTime;
+    use serde_json;
 
-    fn from_str(v: &str) -> Result<Self> {
-        Ok(Self(v.to_string()))
+    #[test]
+    fn serialize_time_string() {
+        assert_eq!(
+            r#""-30m""#,
+            serde_json::to_string(&QueryTime::from("-30m")).unwrap(),
+        );
+    }
+
+    #[test]
+    fn serialize_default() {
+        assert_eq!("0", serde_json::to_string(&QueryTime::default()).unwrap());
+    }
+
+    #[test]
+    fn serialize_timestamp() {
+        assert_eq!(
+            "123",
+            serde_json::to_string(&QueryTime::from(123u64)).unwrap(),
+        );
+    }
+
+    #[test]
+    fn serialize_ms_ago() {
+        assert_eq!(
+            "-123",
+            serde_json::to_string(&QueryTime::from(-123i64)).unwrap()
+        )
     }
 }
